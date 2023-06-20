e23cffd80b0aad58c713182a7afd10f925871631
BlingCc <54300418+BlingCc233@users.noreply.github.com>
2023-06-21 01:53:30 +0800

    Update bilibili.js

--- bilibili.js
+++ bilibili.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bilibili自动全屏脚本
-// @namespace    http://tampermonkey.net/
-// @version      1.1
+// @namespace    bilibili-auto-fullscreen-script
+// @version      1.2
 // @description  在Bilibili网站自动全屏播放视频
 // @author       BlingCc
 // @match        https://www.bilibili.com/*
@@ -37,6 +37,43 @@
                 simulateMouseEvent(fullscreenButton, 'mouseleave', x, y);
             }
             }, 1000);
+            setTimeout(function() {
+                let rect = videoWrap.getBoundingClientRect();
+                let x = rect.left + rect.width / 2;
+                let y = rect.top + rect.height / 2;
+                simulateMouseEvent(videoWrap, 'mouseenter', x, y);
+                simulateMouseEvent(videoWrap, 'mouseover', x, y);
+            // 找到全屏按钮元素
+            let fullscreenButton = document.querySelector('[aria-label="网页全屏"]');
+            if (fullscreenButton) {
+                // 将鼠标移动到该元素上并模拟点击
+                rect = fullscreenButton.getBoundingClientRect();
+                x = rect.left + rect.width / 2;
+                y = rect.top + rect.height / 2;
+                simulateMouseEvent(fullscreenButton, 'mouseenter', x, y);
+                simulateMouseEvent(fullscreenButton, 'click', x, y);
+                simulateMouseEvent(fullscreenButton, 'mouseleave', x, y);
+            }
+            }, 1000);
+            setTimeout(function() {
+
+                let rect = videoWrap.getBoundingClientRect();
+                let x = rect.left + rect.width / 2;
+                let y = rect.top + rect.height / 2;
+                simulateMouseEvent(videoWrap, 'mouseenter', x, y);
+                simulateMouseEvent(videoWrap, 'mouseover', x, y);
+            // 找到全屏按钮元素
+            let fullscreenButton = document.querySelector('[aria-label="网页全屏"]');
+            if (fullscreenButton) {
+                // 将鼠标移动到该元素上并模拟点击
+                rect = fullscreenButton.getBoundingClientRect();
+                x = rect.left + rect.width / 2;
+                y = rect.top + rect.height / 2;
+                simulateMouseEvent(fullscreenButton, 'mouseenter', x, y);
+                simulateMouseEvent(fullscreenButton, 'click', x, y);
+                simulateMouseEvent(fullscreenButton, 'mouseleave', x, y);
+            }
+            }, 4000);
 
         }
     };

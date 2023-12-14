bc7120fcbd8e73a7ce563303a4bfc5095ffa4f6e
BlingCc <54300418+BlingCc233@users.noreply.github.com>
2023-12-15 00:10:21 +0800

    Update uestc.js

--- uestc.js
+++ uestc.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name         UESTC评教
 // @namespace    http://tampermonkey-check-evaIndex-button-uestc-actions
-// @version      1.1.2
+// @version      1.2.1
 // @description  用法：导入油猴。进入评教页，点击右上角按钮。
-// @match        http://eams.uestc/*
+// @match        https://eams.uestc.edu.cn/*
 // @grant        none
 // @author       BlingCc
 // @license      GNU General Public License v3.0 or later
 
+// @downloadURL https://update.greasyfork.org/scripts/468441/UESTC%E8%AF%84%E6%95%99.user.js
+// @updateURL https://update.greasyfork.org/scripts/468441/UESTC%E8%AF%84%E6%95%99.meta.js
 // ==/UserScript==
 
 (function() {
@@ -19,46 +21,39 @@
     button.style.top = '50px';
     button.style.right = '50px';
     button.addEventListener('click', () => {
-        // Check all checkboxes with ID "evaIndex"
+        var idRegex = /^option_(\d+)_0$/;
+        var inputs = document.getElementsByTagName('input');
+
+        for (var i = 0; i < inputs.length; i++) {
+            var input = inputs[i];
+            
+            if (input.id && idRegex.test(input.id)) {
+                
+                var num = idRegex.exec(input.id)[1];
+                
+                if (num >= 0 && num <= 100) {  // 这里可以根据你的具体条件进行修改
+                    input.checked = true;
+                }
+            }
+        }
+        
         const checkboxes = document.querySelectorAll('input[type="checkbox"][id="evaIndex"]');
         checkboxes.forEach((checkbox) => {
             checkbox.checked = true;
         });
 
-        // Click the fifth li element in all ul elements inside td elements whose ID matches the pattern "starTd_数字"
-        const tdElements = document.querySelectorAll('td[id^="starTd_"]');
-        tdElements.forEach((td) => {
-            const ulElements = td.querySelectorAll('ul');
-            ulElements.forEach((ul) => {
-                const liElements = ul.querySelectorAll('li');
 
-                const li5 = liElements[4]
-                var rect = li5.getBoundingClientRect();
-                var x = rect.left + (rect.width / 2);
-                var y = rect.top + (rect.height / 2);
 
-                var event = new MouseEvent('click', {
-                    view: window,
-                    bubbles: true,
-                    cancelable: true,
-                    clientX: x,
-                    clientY: y
-                });
-                setTimeout(function() {
-                    li5.dispatchEvent(event);
-                }, 0);
-
-                if (liElements.length >= 5) {
-                    liElements[4].click();
-                }
-            });
-        });
-
-        // Fill in the input box with ID "evaText" with a random number
         const inputBox = document.querySelector('textarea[id="evaText"]');
+        var fourthTdContent = document.querySelector('body > div#BodyBg > div#MainBody.bg1 > div#main.ajax_container > form > table.infoTitle2 > tbody > tr:nth-child(2) > td:nth-child(4)').textContent;
         if (inputBox) {
             const randomNumber = Math.floor(Math.random() * 301);
-            inputBox.value = '我认为老师讲的好得很，认真学习就能……' + randomNumber.toString();
+            inputBox.value = '我认为'+fourthTdContent.toString()+'老师讲的好得很，认真学习就能……' + randomNumber.toString();
+        }
+
+        var submitButton = document.getElementById('sub');
+        if (submitButton) {
+            submitButton.click();
         }
     });
 

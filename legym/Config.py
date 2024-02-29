80433ef219dc56369ed5167c459bb08e9e00bb74
BlingCc <54300418+BlingCc233@users.noreply.github.com>
2024-02-29 16:51:33 +0800

    Create Config.py

---
+++ legym/Config.py
@@ -0,0 +1,10 @@
+headers = {
+    "Content-Type": "application/json",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Organization": "",
+    "Connection": "keep-alive",
+    "Accept": "*/*",
+    "User-Agent": "",
+    "Authorization": "",
+    "Accept-Language": ""
+}

437d7223ad83f36a033cfddc7c68b8ffc408a81c
BlingCc <54300418+BlingCc233@users.noreply.github.com>
2024-02-29 17:48:12 +0800

    Update Config.py

--- legym/Config.py
+++ legym/Config.py
@@ -1,10 +1,2 @@
-headers = {
-    "Content-Type": "application/json",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Organization": "",
-    "Connection": "keep-alive",
-    "Accept": "*/*",
-    "User-Agent": "",
-    "Authorization": "",
-    "Accept-Language": ""
-}
+username = ""
+password = ""
